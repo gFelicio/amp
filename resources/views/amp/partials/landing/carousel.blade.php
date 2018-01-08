01c757f5502b03cf02ccdf900f0b4014db85d7bb
gFelicio <gustavo.a.felicio@gmail.com>
2018-01-08 12:05:21 -0200

    styling

--- resources/views/amp/partials/landing/carousel.blade.php
+++ resources/views/amp/partials/landing/carousel.blade.php
@@ -30,3 +30,14 @@
         <amp-img src="{{ asset('img/img_9.png') }}" width="400" height="300" layout="responsive"></amp-img>
     </amp-carousel>
 </div>
+
+<div class="txtDiv">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</div>
