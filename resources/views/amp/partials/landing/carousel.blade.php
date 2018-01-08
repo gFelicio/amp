a562e43dd82c4df33b5f33ee2199e7da8f77f185
gFelicio <gustavo.a.felicio@gmail.com>
2018-01-08 12:42:17 -0200

    fleshing out carousel bit

--- resources/views/amp/partials/landing/carousel.blade.php
+++ resources/views/amp/partials/landing/carousel.blade.php
@@ -1,11 +1,9 @@
 <div class="txtDiv">
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Another web page component that devs love to implement in their page (I don't know why, because it sucks in many ways), is the carousel. You know, that thing that changes images every X seconds, with the arrows where you can control the pace of these images and so on. Yu know this stuff, right?
+    </p>
+    <p>
+        Anyway, AMP created a SEPARATE component so web devs can implement these carousels in their pages. Even though I don't really like this component, either created by devs or by AMP, I know there are people that like them, their functionality and also that a lot of pages use it to advertise their own articles or any other thing they want. So, basically, keep in mind, people love this stuff. They use it in their stuff and all.
     </p>
 </div>
 
