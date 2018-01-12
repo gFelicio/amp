c61ed397e2b9af43a9a2296ae831f0b48f3d7254
gFelicio <gustavo.a.felicio@gmail.com>
2018-01-12 14:57:03 -0200

    optimize scss

--- resources/views/amp/partials/landing/info.blade.php
+++ resources/views/amp/partials/landing/info.blade.php
@@ -7,43 +7,7 @@
         Gustavo Felício
     </span>
 
-    <span>
+    <small>
         04/01/2018
-    </span>
+    </small>
 </div>
-
-{{-- <div class="imgDiv">
-    <amp-img src="{{ asset('img/img_1.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_2.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_3.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_4.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_5.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_6.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_7.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_8.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div>
-
-<div class="imgDiv">
-    <amp-img src="{{ asset('img/img_9.png') }}" layout="responsive" width="500" height="250"></amp-img>
-</div> --}}
